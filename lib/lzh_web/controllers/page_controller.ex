04487778f5e1d22d4bb7c33e4bb6b13ee6c8dc3a
pavelsof <mail@pavelsof.com>
2023-10-07 21:58:40 +0200

    Highlight the active nav link.

--- lib/lzh_web/controllers/page_controller.ex
+++ lib/lzh_web/controllers/page_controller.ex
@@ -8,30 +8,35 @@ defmodule LzhWeb.PageController do
   def home(conn, _params) do
     conn
     |> assign(:page_title, "Начало")
+    |> assign(:nav_item, :home)
     |> render(:home)
   end
 
   def about(conn, _params) do
     conn
     |> assign(:page_title, "За проекта")
+    |> assign(:nav_item, :about)
     |> render(:about)
   end
 
   def methodology(conn, _params) do
     conn
     |> assign(:page_title, "Методика")
+    |> assign(:nav_item, :methodology)
     |> render(:methodology)
   end
 
   def team(conn, _params) do
     conn
     |> assign(:page_title, "Екип")
+    |> assign(:nav_item, :team)
     |> render(:team)
   end
 
   def faq(conn, _params) do
     conn
     |> assign(:page_title, "Често задавани въпроси")
+    |> assign(:nav_item, :faq)
     |> render(:faq)
   end
 
