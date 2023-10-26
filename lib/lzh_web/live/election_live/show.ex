7f5745ed3e71fef7076c0f8b6c866a78b6246a8d
pavelsof <mail@pavelsof.com>
2023-10-26 13:47:35 +0300

    Election live: dropdowns on mobile.

--- lib/lzh_web/live/election_live/show.ex
+++ lib/lzh_web/live/election_live/show.ex
@@ -3,6 +3,10 @@ defmodule LzhWeb.ElectionLive.Show do
 
   alias Lzh.{Elections, Statements}
 
+  #
+  # life cycle
+  #
+
   @impl true
   def mount(:not_mounted_at_router, %{"election" => election_id}, socket) do
     election = Elections.get_election!(election_id)
@@ -34,6 +38,10 @@ defmodule LzhWeb.ElectionLive.Show do
 
     {:ok, socket}
   end
+
+  #
+  # event handlers
+  #
 
   @impl true
   def handle_event("toggle_party", %{"party" => toggled_party_id}, socket) do
@@ -94,7 +102,81 @@ defmodule LzhWeb.ElectionLive.Show do
   end
 
   @impl true
+  def handle_event("select_party", %{"party" => party_id}, socket) do
+    %{statements: statements} = socket.assigns
+
+    party_id = String.to_integer(party_id)
+
+    socket =
+      if party_id == 0 do
+        socket
+        |> assign(:selected_all, true)
+        |> assign(:selected_party_id, nil)
+        |> assign(:selected_politician_id, nil)
+        |> assign(:selected_statements, statements)
+      else
+        selected_statements =
+          Enum.filter(statements, fn statement ->
+            statement.politician.party.id == party_id
+          end)
+
+        socket
+        |> assign(:selected_all, false)
+        |> assign(:selected_party_id, party_id)
+        |> assign(:selected_politician_id, nil)
+        |> assign(:selected_statements, selected_statements)
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("select_politician", %{"politician" => politician_id}, socket) do
+    %{politicians: politicians, statements: statements} = socket.assigns
+
+    politician_id = String.to_integer(politician_id)
+
+    socket =
+      if politician_id == 0 do
+        socket
+        |> assign(:selected_all, true)
+        |> assign(:selected_politician_id, nil)
+        |> assign(:selected_statements, statements)
+      else
+        selected_statements =
+          Enum.filter(statements, fn statement ->
+            statement.politician.id == politician_id
+          end)
+
+        party_id =
+          politicians
+          |> Enum.find(&(&1.id == politician_id))
+          |> Map.get(:party_id)
+
+        socket
+        |> assign(:selected_all, false)
+        |> assign(:selected_party_id, party_id)
+        |> assign(:selected_politician_id, politician_id)
+        |> assign(:selected_statements, selected_statements)
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def handle_event(_event, _params, socket) do
     {:noreply, socket}
+  end
+
+  #
+  # view functions
+  #
+
+  def filter_by_party(politicians, party_id) when is_integer(party_id) do
+    Enum.filter(politicians, &(&1.party_id == party_id))
+  end
+
+  def filter_by_party(politicians, nil) do
+    politicians
   end
 end
